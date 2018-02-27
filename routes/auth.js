63ed471824c82c32a6d26c9e3eb423e8281004bc
Dave Hanagan <billyxshenaniganfilms@hotmail.com>
2018-02-26 22:34:11 -0800

    Full functionality for generating a valid dev token

--- routes/auth.js
+++ routes/auth.js
@@ -4,7 +4,22 @@ const token = require('../lib/token');
 
 router.get('/token', (req, res) => {
   return token.sign()
-  .then(token => res.json(token));
+  .then(token => res.json(token))
+  .catch( err => {
+    res.status(500).json({ reason: 'Token Signing failure'});
+  });
 });
+
+router.post('/decode', jsonParser, (req, res) => {
+  const requestToken = req.body.token;
+
+  return token.decode(requestToken)
+  .then( object => {
+    res.json(object);
+  })
+  .catch( err => {
+    res.status(500).json({ reason: 'Token Decoding failure'});
+  });
+})
 
 module.exports = router;
